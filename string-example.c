f50619dee4d3aa36b045397be0922c03fb41cf08
Jared Krinke <jared.krinke@gmail.com>
2021-10-04 18:04:38 -0700

    Working example

--- string-example.c
+++ string-example.c
@@ -3,6 +3,14 @@
 #define WASM_EXPORT_AS(name) __attribute__((export_name(name)))
 #define WASM_EXPORT(symbol) WASM_EXPORT_AS(#symbol) symbol
 
+unsigned char* WASM_EXPORT(allocate)(unsigned int size) {
+    return (unsigned char*)malloc(size);
+}
+
+void WASM_EXPORT(deallocate)(unsigned char* allocation) {
+    free(allocation);
+}
+
 unsigned int WASM_EXPORT(countAs)(const char* string) {
     unsigned int numberOfAs = 0;
     while (*string != '\0') {
